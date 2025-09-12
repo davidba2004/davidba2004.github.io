bd8bae5a732e12e5d06e0e922063cc9b7c57f401
David Arrington <davidba@email.sc.edu>
2025-09-12 15:11:31 -0400

    example: js continued

--- csce242/examples/js/js/intro.js
+++ csce242/examples/js/js/intro.js
@@ -6,6 +6,26 @@ const sayHello = () => {
 document.getElementById("btn-click-me").onclick = sayHello;
 */
 
-document.getElementById("btn-click-me").onclick = () => {
-    console.log("Ws good");
+document.getElementById("happy-btn").onclick = (event) => {
+    document.getElementById("p-welcome").innerHTML = "Yay!";
+    event.currentTarget.classList.add("happy"); // current target is the button that was clicked
+    document.getElementById('sad-btn').classList.remove("sad");
 };
+
+document.getElementById("sad-btn").onclick = (event) => {
+    document.getElementById("p-welcome").innerHTML = "Nay!";
+    event.currentTarget.classList.add("sad"); // current target is the button that was clicked
+    document.getElementById("happy-btn").classList.remove("happy");
+};
+
+document.getElementById("clear-btn").onclick = () => {
+    document.getElementById("p-welcome").innerHTML = "";
+    document.getElementById('sad-btn').classList.remove("sad");
+    document.getElementById("happy-btn").classList.remove("happy");
+};
+
+document.getElementById("txt-emotion").onkeyup = (event) => {
+    const userInput = event.currentTarget.value;
+    document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}.`;
+    document.getElementById('img-emotion').classList.remove("hidden");
+}
