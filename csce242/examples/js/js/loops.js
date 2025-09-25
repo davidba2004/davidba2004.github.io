9b1114973863ca734bdd7bc9b097a03c7d6083c7
David Arrington <davidba@email.sc.edu>
2025-09-25 12:39:00 -0400

    examples: js arrays

--- csce242/examples/js/js/loops.js
+++ csce242/examples/js/js/loops.js
@@ -26,6 +26,61 @@ document.getElementById("btn-count").onclick = () => {
     resultDiv.innerHTML = "";
 
     for (let i = start; i <= end; i++) {
+        const p = document.createElement("p");
+        p.innerHTML = i.toString();
+        document.getElementById("d-count").append(p);
+        p.onclick = () => {
+            console.log(`You clicked number ${i}`)
+        }
+    }
+}
 
+document.getElementById("btn-toys").onclick = () => {
+    const toys = document.getElementById("toys-list");
+    toys.innerHTML = "";
+
+    const ts = ["ball", "skipping rope", "doll", "mini car", "elmo"]
+
+    ts.forEach((t, i) => {
+        const li = document.createElement("li");
+        li.innerHTML = `${i}. ${t}`;
+        toys.appendChild(li);
+    })
+
+    /* OR
+    for (const t of ts) {
+        const li = document.createElement("li");
+        li.innerHTML = t;
+        toys.appendChild(li);
+    }
+     */
+
+    /* OR
+     for (int i = 0; i < ts.length; i++ {
+        const li = document.createElement("li);
+        li.innerHTML = ts[i];
+        toys.appendChild(li);
+     }
+     */
+}
+
+document.getElementById("btn-toy-prices").onclick = () => {
+    const toys = []
+    toys["ball"] = 2.99;
+    toys["barbie"] = 19.99;
+    toys["skipping rope"] = 1.98;
+    toys["match box car"] = 2.54;
+
+    const toyDiv = document.getElementById("toys-prices-top");
+    const priceP = document.getElementById("toys-prices-bottom");
+
+    for (let toy in toys) {
+        const p = document.createElement("p");
+        p.innerHTML = `${toy}`;
+        toyDiv.append(p);
+
+        p.onclick = () => {
+            priceP.innerHTML = `${toy} costs $${toys[toy]}`;
+        }
     }
 }
